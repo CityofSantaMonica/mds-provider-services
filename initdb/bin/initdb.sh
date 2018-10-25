77fe47beefd1a7fa69f6645f36ebae3dc959e70d
Kegan Maher <kegan.maher@smgov.net>
2018-10-25 15:11:28 -0700

    move the user/db creation

--- initdb/bin/initdb.sh
+++ initdb/bin/initdb.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 set -e
 
-# create the MDS user and database
-
-export PGUSER=$POSTGRES_USER
-export PGPASSWORD=$POSTGRES_PASSWORD
-
-psql -v ON_ERROR_STOP=1 --host "$POSTGRES_HOSTNAME" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER $MDS_USER WITH PASSWORD '$MDS_PASSWORD';
-
-    CREATE DATABASE $MDS_DB
-        WITH OWNER $MDS_USER
-        TEMPLATE 'template_postgis'
-        ENCODING 'UTF8'
-        CONNECTION LIMIT -1
-    ;
-EOSQL
-
 # run the MDS setup scripts
 
 export PGUSER=$MDS_USER
