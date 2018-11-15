3c1f360b48dc223ea53e05080894616df1e7eff9
Kegan Maher <kegan.maher@smgov.net>
2018-11-14 22:25:50 -0800

    tighten the status_changes uniqueness constraint
    do not consider event_type and event_type_reason
    a given provider's device should never have 2 simultaneous events

--- db/migrate/0.2.0.sql
+++ db/migrate/0.2.0.sql
@@ -3,6 +3,13 @@ ALTER TABLE status_changes
     SET DATA TYPE jsonb
     USING event_location::jsonb;
 
+ALTER TABLE status_changes
+    DROP CONSTRAINT unique_event;
+
+ALTER TABLE status_changes
+    ADD CONSTRAINT unique_provider_device_event
+    UNIQUE (provider_id, device_id, event_time);
+
 ALTER TABLE trips
     ALTER COLUMN route
     SET DATA TYPE jsonb
