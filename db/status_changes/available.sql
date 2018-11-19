b680ce9c90fe2f08a6f37e10881871f9ce2bd077
Kegan Maher <kegan.maher@smgov.net>
2018-11-19 11:17:07 -0800

    adding event_location geometry

--- db/status_changes/available.sql
+++ db/status_changes/available.sql
@@ -1,8 +1,8 @@
--- A view of service_start Status Changes.
+-- A view of event_type: 'available' Status Changes.
 
 CREATE MATERIALIZED VIEW public.v_status_changes_available AS
 
-SELECT
+  SELECT
     provider_id,
     provider_name,
     device_id,
@@ -12,13 +12,14 @@ SELECT
     event_type,
     event_type_reason,
     event_time,
+    event_location -> 'properties' ->> 'timestamp'::timestamptz at time zone 'UTC' as event_timestamp,
     timezone('America/Los_Angeles'::text, event_time) AS event_time_local,
     event_location,
-    event_location::jsonb as event_location_jsonb,
+    st_setsrid(st_geomfromgeojson((event_location -> 'geometry'::text)::text), 4326) AS event_location_geom,
     battery_pct,
     associated_trips
-FROM status_changes
-WHERE event_type = 'available'
-ORDER BY event_time_local DESC, provider_name
+  FROM status_changes
+  WHERE event_type = 'available'::event_types
+  ORDER BY (timezone('America/Los_Angeles'::text, event_time)) DESC, provider_name
 
 WITH NO DATA;
