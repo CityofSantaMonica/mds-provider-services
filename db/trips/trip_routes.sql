ab97c8fc9e7832855540c211998c1f1d3e5829af
Kegan Maher <kegan.maher@smgov.net>
2018-11-20 17:59:27 -0800

    unrolling trip routes w/geometry

---
+++ db/trips/trip_routes.sql
@@ -0,0 +1,26 @@
+
+CREATE MATERIALIZED VIEW public.v_trip_routes AS
+
+SELECT
+    provider_name,
+    device_id,
+    vehicle_type,
+    trip_id,
+    start_time AS trip_start_time,
+    timezone('America/Los_Angeles', start_time) AS trip_start_time_local,
+    end_time AS trip_end_time,
+    timezone('America/Los_Angeles', end_time) AS trip_end_time_local,
+    timezone('America/Los_Angeles', to_timestamp(((jsonb_array_elements(route -> 'features') -> 'properties') ->> 'timestamp')::numeric) AT TIME ZONE 'UTC') AS timepoint_local,
+    st_setsrid(st_geomfromgeojson(jsonb_array_elements(route -> 'features') ->> 'geometry'), 4326) AS geometry,
+    accuracy AS route_accuracy,
+    trip_distance,
+    trip_duration
+FROM
+    trips
+ORDER BY
+    trip_end_time_local DESC, trip_id, timepoint_local
+
+WITH NO DATA;
+
+ALTER TABLE public.v_trip_routes
+    OWNER TO mds_provider;
