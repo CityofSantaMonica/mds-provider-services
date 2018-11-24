d063501a9b41e8a1c5256f3b3a492b04a4b23879
Kegan Maher <kegan.maher@smgov.net>
2018-11-24 12:45:49 -0800

    helper functions that actually help
    - wrap up needed geometries in functions that compute
      from the geoJSON directly. Clearly I am no GIS expert.
    
    - timestamp conversion
    
    - json feature -> geometry parser
    
    - namespaced with csm_
    
    - IMMUTABLE PARALLEL SAFE inherited
      from underlying st_* functions

---
+++ db/functions/local_timestamp.sql
@@ -0,0 +1,21 @@
+-- timestamp -> timestamptz
+CREATE OR REPLACE FUNCTION public.csm_local_timestamp(timestamp = clock_timestamp())
+    RETURNS timestamptz
+    LANGUAGE 'sql'
+    STABLE
+AS $BODY$
+
+SELECT timezone('America/Los_Angeles', $1)
+
+$BODY$;
+
+-- timestamptz -> timestamp
+CREATE OR REPLACE FUNCTION public.csm_local_timestamp(timestamptz = clock_timestamp())
+    RETURNS timestamp
+    LANGUAGE 'sql'
+    STABLE
+AS $BODY$
+
+SELECT timezone('America/Los_Angeles', $1)
+
+$BODY$;
---
+++ db/functions/parse_feature_geom.sql
@@ -0,0 +1,21 @@
+-- json input
+CREATE OR REPLACE FUNCTION public.csm_parse_feature_geom(json)
+    RETURNS geometry
+    LANGUAGE 'sql'
+    IMMUTABLE PARALLEL SAFE
+AS $BODY$
+
+SELECT st_setsrid(st_geomfromgeojson(($1 -> 'geometry'::text)::text), 4326)
+
+$BODY$;
+
+-- jsonb input
+CREATE OR REPLACE FUNCTION public.csm_parse_feature_geom(jsonb)
+    RETURNS geometry
+    LANGUAGE 'sql'
+    IMMUTABLE PARALLEL SAFE
+AS $BODY$
+
+SELECT st_setsrid(st_geomfromgeojson(($1 -> 'geometry'::text)::text), 4326)
+
+$BODY$;
