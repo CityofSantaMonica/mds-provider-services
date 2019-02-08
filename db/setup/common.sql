cccc98d0095ae4b87c04fc15c0f54b5a77990ee0
Kegan Maher <kegan.maher@smgov.net>
2019-02-08 11:42:35 -0800

    drop types before create

--- db/setup/common.sql
+++ db/setup/common.sql
@@ -1,7 +1,11 @@
+DROP TYPE IF EXISTS public.vehicle_types CASCADE;
+
 CREATE TYPE public.vehicle_types AS ENUM (
     'bicycle',
     'scooter'
 );
+
+DROP TYPE IF EXISTS public.propulsion_types CASCADE;
 
 CREATE TYPE public.propulsion_types AS ENUM (
     'human',
@@ -10,12 +14,16 @@ CREATE TYPE public.propulsion_types AS ENUM (
     'combustion'
 );
 
+DROP TYPE IF EXISTS public.event_types CASCADE;
+
 CREATE TYPE public.event_types AS ENUM (
     'available',
     'reserved',
     'unavailable',
     'removed'
 );
+
+DROP TYPE IF EXISTS public.event_type_reasons CASCADE;
 
 CREATE TYPE public.event_type_reasons AS ENUM (
     'service_start',
